fd9176fce1e6465884c88537cb2eb35f42955379
KeithAsazawa <keith.asazawa@gmail.com>
2013-09-11 15:15:14 -0700

    First commit add Pages controller

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Oakland6558simson::Application.config.secret_token = 'c0cbf0875b177ea132be2a7861b45e9efd09e7b2d98710e4dd2e8a9ea618f421d74e459fd4b5e62749a930146f9b078936d437eacc0a12fda1c4f1b891d20a09'
